6407b7d189e8d7192e12c951e03da337b6902a05
cainalopes1805 <cainalopes1@gmail.com>
2026-08-24 22:15:06 -0300

    oi

---
+++ aula6/auth.js
@@ -0,0 +1,8 @@
+const btAddUser = document.getElementById("idAddUser");
+btAddUser?.addEventListener("click", (e) => {
+
+    const modalRegister = document.getElementById("idModalRegister");
+    modalRegister.classList.remove("hidden");
+    modalRegister.classList.add("flex")
+
+});
